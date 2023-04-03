c12e25409d29979469b6fb5fc5b5213aeda3c27f
Rahul Anand <104441346+rinku435@users.noreply.github.com>
2023-04-03 17:29:20 +0530

    add menu complete implementation

--- src/app/builder/application-list/add-application/manage-menu/add-menu/add-menu.component.ts
+++ src/app/builder/application-list/add-application/manage-menu/add-menu/add-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { MenuModel } from 'src/app/application-model/menu-model';
 
@@ -8,7 +8,10 @@ import { MenuModel } from 'src/app/application-model/menu-model';
   styleUrls: ['./add-menu.component.css']
 })
 export class AddMenuComponent implements OnInit {
-  menuItems:MenuModel[] = []
+  menuItems:MenuModel[] = [];
+
+  @Output('onCancel') onCancel = new EventEmitter();
+  @Output('onSave') onSave = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
@@ -20,14 +23,24 @@ export class AddMenuComponent implements OnInit {
     }
     item.items.push(new MenuModel());
   }
+
   addNewParent() {
     this.menuItems.push(new MenuModel('', '','',[]));
   }
+
   deleteNode(item:MenuItem,index:number) {
     if(item) {
       item.items?.splice(index, 1);
     } else {
       this.menuItems.splice(index, 1);
     }
+  }
+
+  cancel() {
+    this.onCancel.emit('cancel');
+  }
+
+  save() {
+    this.onSave.emit(this.menuItems);
   }
 }
