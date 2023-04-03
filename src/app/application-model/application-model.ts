c841b7206197c5453939daefd57197181b12c8db
Aswini-1 <aswini.biswal@bipros.com>
2023-04-03 16:44:05 +0530

    application component updated

--- src/app/application-model/application-model.ts
+++ src/app/application-model/application-model.ts
@@ -1,10 +1,10 @@
 export class ApplicationModel {
-  id:string;
-  name:string;
-  icon:string;
-  description:string;
+  id?: string;
+  name?: string;
+  icon?: string;
+  description?: string;
 
-  constructor(id:string, name: string, icon : string, description:string){
+  constructor(id?: string, name?: string, icon?: string, description?: string) {
     this.id = id;
     this.name = name;
     this.icon = icon;
