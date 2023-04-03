11beb7c6cf4498d5c390f8ce35a6e99773ff1f8d
Rahul Anand <104441346+rinku435@users.noreply.github.com>
2023-04-03 11:25:29 +0530

    application component added

--- src/app/builder/application-list/application/application.component.ts
+++ src/app/builder/application-list/application/application.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ApplicationModel } from 'src/app/application-model/application-model';
 
 @Component({
   selector: 'app-application',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./application.component.css']
 })
 export class ApplicationComponent implements OnInit {
-
+  @Input('application') application: ApplicationModel = new ApplicationModel('001','App Manage','pi pi-plus','description');
   constructor() { }
 
   ngOnInit(): void {
