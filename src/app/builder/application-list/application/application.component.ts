7f7fe38c91f2f7c6d2959b6bac593afe438b1a4f
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2023-04-03 13:57:53 +0530

    updated
    updated

--- src/app/builder/application-list/application/application.component.ts
+++ src/app/builder/application-list/application/application.component.ts
@@ -5,19 +5,21 @@ import { ApplicationModel } from 'src/app/application-model/application-model';
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
-  styleUrls: ['./application.component.css']
+  styleUrls: ['./application.component.css'],
 })
 export class ApplicationComponent implements OnInit {
-  @Input('application') application: ApplicationModel = new ApplicationModel('001','App Manage','pi pi-plus','description');
-  @Input('bgColor') bgColor:string = "";
-  
-  constructor() { }
+  @Input('application') application: ApplicationModel = new ApplicationModel(
+    '001',
+    'App Manage',
+    'pi pi-plus',
+    'description'
+  );
+  @Input('bgColor') background: string = '';
 
-  ngOnInit(): void {
+  constructor() {}
 
+  ngOnInit(): void {}
+  getColor() {
+    return {background : this.background};
   }
-
-  getRandomColor(){
-  }
-
 }
