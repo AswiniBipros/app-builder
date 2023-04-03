221491666a745345c70c198fb8cea7e05ad6e50c
Aswini-1 <aswini.biswal@bipros.com>
2023-04-03 15:55:54 +0530

    updated menu-model

--- src/app/application-model/menu-model.ts
+++ src/app/application-model/menu-model.ts
@@ -1,11 +1,13 @@
 export class MenuModel {
-  id:string;
-  name:string;
-  icon:string;
+  id: string;
+  name: string;
+  icon: string;
+  children: MenuModel[] = [];
 
-  constructor(id:string, name: string, icon : string){
+  constructor(id: string, name: string, icon: string, children: MenuModel[]) {
     this.id = id;
     this.name = name;
     this.icon = icon;
+    this.children = children;
   }
 }
